d8761be13800d7814ba6f6db32d75f659c03ab22
Roman Karavia <3082173+rkaravia@users.noreply.github.com>
2022-11-05 15:13:31 +0100

    Highlight on click

--- map-with-flags.js
+++ map-with-flags.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const state = {
+  highlightId: null
+}
+
 const cantonPatterns = createCantonPatterns();
 
 const map = initializeMap();
@@ -10,7 +14,10 @@ map.addLayer(backgroundMap);
 const overlay = cantonalBoundaries(cantonPatterns);
 map.addLayer(overlay);
 
-mouseMoveRevealCircle(map, overlay);
+overlay.on('click', event => {
+  state.highlightId = event.layer.properties.name;
+  overlay.setStyle(makeCantonStyle(cantonPatterns, state.highlightId));
+});
 
 function createCantonPatterns() {
   const cantons = { 'Aargau': 'AG', 'Appenzell Innerrhoden': 'AI', 'Appenzell Ausserrhoden': 'AR', 'Bern': 'BE', 'Basel-Landschaft': 'BL', 'Basel-Stadt': 'BS', 'Fribourg': 'FR', 'Genève': 'GE', 'Glarus': 'GL', 'Graubünden': 'GR', 'Jura': 'JU', 'Luzern': 'LU', 'Neuchâtel': 'NE', 'Nidwalden': 'NW', 'Obwalden': 'OW', 'St. Gallen': 'SG', 'Schaffhausen': 'SH', 'Solothurn': 'SO', 'Schwyz': 'SZ', 'Thurgau': 'TG', 'Ticino': 'TI', 'Uri': 'UR', 'Vaud': 'VD', 'Valais': 'VS', 'Zug': 'ZG', 'Zürich': 'ZH' };
@@ -45,7 +52,7 @@ function initializeMap() {
   const element = document.createElement('div');
   document.body.appendChild(element);
 
-  const map = L.map(element, { crs: L.CRS.EPSG2056 });
+  const map = L.map(element, { crs: L.CRS.EPSG2056, minZoom: 14 });
 
   map.fitSwitzerland();
 
@@ -57,76 +64,38 @@ function swisstopoGrayMap() {
 }
 
 function cantonalBoundaries(cantonPatterns) {
-  return L.vectorGrid.protobuf('cantonal-boundaries/{z}/{x}/{y}.pbf', {
+  return new VectorTileLayer('cantonal-boundaries/{z}/{x}/{y}.pbf', {
     attribution: `
       <a href="https://github.com/nzzdev/ch-canton-symbols">NZZ Storytelling</a>
       (<a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a>),
       <a href="https://github.com/rkaravia/map-with-flags">Code on GitHub</a>
     `,
-    maxNativeZoom: 20,
-    vectorTileLayerStyles: {
-      swissboundaries3d_1_3_tlm_kantonsgebiet(properties, zoom) {
-        let patternSize = 0;
-        let weight = 1;
-        if (zoom > 20) {
-          patternSize = 2;
-          weight = 5;
-        } else if (zoom > 15) {
-          patternSize = 1;
-          weight = 2;
-        }
-        return {
-          color: '#000',
-          fill: true,
-          fillColor: cantonPatterns[properties.name][patternSize],
-          fillOpacity: 1,
-          weight: weight * window.devicePixelRatio,
-        };
-      },
+    getFeatureId(feature) {
+      return feature.properties.name
     },
+    maxNativeZoom: 20,
+    style: makeCantonStyle(cantonPatterns)
   });
 }
 
-function mouseMoveRevealCircle(map, gridLayer) {
-  const tileSize = gridLayer.getTileSize();
-  const radius = 128;
-  const maskSize = 2 * (tileSize.x + radius);
-  const maskCenter = L.point(maskSize, maskSize).divideBy(2);
-  const mask = createMask(maskSize, radius);
-  map.on('mousemove', (event) => {
-    const origin = map.getPixelOrigin();
-    const { layerPoint } = event;
-    const scale = map.getZoomScale(map.getZoom(), gridLayer._level.zoom);
-    Object.values(gridLayer._tiles).forEach((tile) => {
-      const { style } = tile.el;
-      const tilePosition = tile.coords.scaleBy(tileSize).multiplyBy(scale).subtract(origin);
-      const offset = layerPoint.subtract(tilePosition).divideBy(scale);
-      if (-radius < offset.x && offset.x < tileSize.x + radius &&
-          -radius < offset.y && offset.y < tileSize.y + radius) {
-        const maskPosition = offset.subtract(maskCenter);
-        style['-webkit-mask-image'] = `url(${mask})`;
-        style['-webkit-mask-position'] = `${maskPosition.x}px ${maskPosition.y}px`;
-      }
-      else {
-        style['-webkit-mask-image'] = '';
-      }
-    });
-  });
-}
-
-function createMask(size, radius) {
-  const canvas = document.createElement('canvas');
-  canvas.width = canvas.height = size;
-  const context = canvas.getContext('2d');
-
-  const center = size / 2;
-  var gradient = context.createRadialGradient(center, center, 0, center, center, radius);
-
-  gradient.addColorStop(.9, 'rgb(0, 0, 0, 0)');
-  gradient.addColorStop(.95, '#000');
-
-  context.fillStyle = gradient;
-  context.fillRect(0, 0, size, size);
-
-  return canvas.toDataURL();
+function makeCantonStyle(cantonPatterns, highlightId) {
+  return (feature, layerName, zoom) => {
+    const id = feature.properties.name;
+    let patternSize = 0;
+    let weight = 1;
+    if (zoom > 20) {
+      patternSize = 2;
+      weight = 5;
+    } else if (zoom > 15) {
+      patternSize = 1;
+      weight = 2;
+    }
+    return {
+      color: '#000',
+      fill: highlightId !== id,
+      fillColor: cantonPatterns[id][patternSize],
+      fillOpacity: 1,
+      weight: weight * window.devicePixelRatio,
+    };
+  };
 }
