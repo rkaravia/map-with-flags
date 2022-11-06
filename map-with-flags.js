bc48ac1cea8f40186f891e6f9014345ae746c636
Roman Karavia <3082173+rkaravia@users.noreply.github.com>
2022-11-06 22:43:48 +0100

    Show flag on highlight overlay

--- map-with-flags.js
+++ map-with-flags.js
@@ -1,29 +1,30 @@
 'use strict';
 
+const cantonIdByName = { 'Aargau': 'AG', 'Appenzell Innerrhoden': 'AI', 'Appenzell Ausserrhoden': 'AR', 'Bern': 'BE', 'Basel-Landschaft': 'BL', 'Basel-Stadt': 'BS', 'Fribourg': 'FR', 'Genève': 'GE', 'Glarus': 'GL', 'Graubünden': 'GR', 'Jura': 'JU', 'Luzern': 'LU', 'Neuchâtel': 'NE', 'Nidwalden': 'NW', 'Obwalden': 'OW', 'St. Gallen': 'SG', 'Schaffhausen': 'SH', 'Solothurn': 'SO', 'Schwyz': 'SZ', 'Thurgau': 'TG', 'Ticino': 'TI', 'Uri': 'UR', 'Vaud': 'VD', 'Valais': 'VS', 'Zug': 'ZG', 'Zürich': 'ZH' };
+
 const state = {
-  highlightId: null
+  highlightCanton: null
 }
 
 const cantonPatterns = createCantonPatterns();
 
 const map = initializeMap();
 
-const backgroundMap = swisstopoGrayMap();
-map.addLayer(backgroundMap);
+const backgroundLayer = swisstopoGrayMap();
+map.addLayer(backgroundLayer);
 
-const overlay = cantonalBoundaries(cantonPatterns);
-map.addLayer(overlay);
+const overlayLayer = cantonalBoundaries(cantonPatterns);
+map.addLayer(overlayLayer);
 
-overlay.on('click', event => {
-  state.highlightId = event.layer.properties.name;
-  overlay.setStyle(makeCantonStyle(cantonPatterns, state.highlightId));
-});
+highlightOnClick(overlayLayer, state);
+
+updateOverlay(overlayLayer, state);
 
 function createCantonPatterns() {
-  const cantons = { 'Aargau': 'AG', 'Appenzell Innerrhoden': 'AI', 'Appenzell Ausserrhoden': 'AR', 'Bern': 'BE', 'Basel-Landschaft': 'BL', 'Basel-Stadt': 'BS', 'Fribourg': 'FR', 'Genève': 'GE', 'Glarus': 'GL', 'Graubünden': 'GR', 'Jura': 'JU', 'Luzern': 'LU', 'Neuchâtel': 'NE', 'Nidwalden': 'NW', 'Obwalden': 'OW', 'St. Gallen': 'SG', 'Schaffhausen': 'SH', 'Solothurn': 'SO', 'Schwyz': 'SZ', 'Thurgau': 'TG', 'Ticino': 'TI', 'Uri': 'UR', 'Vaud': 'VD', 'Valais': 'VS', 'Zug': 'ZG', 'Zürich': 'ZH' };
   const patternRoot = document.querySelector('.pattern-svg');
-  Object.entries(cantons).forEach(([name, id]) => {
-    cantons[name] = [0, 1, 2].map(scale => {
+  const patterns = {};
+  Object.entries(cantonIdByName).forEach(([name, id]) => {
+    patterns[name] = [0, 1, 2].map(scale => {
       const factor = 2 ** scale;
       const patternId = `pattern-${id}-${scale}`;
       const pattern = document.createElementNS('http://www.w3.org/2000/svg', 'pattern');
@@ -40,12 +41,12 @@ function createCantonPatterns() {
       const image = document.createElementNS('http://www.w3.org/2000/svg', 'image');
       image.setAttributeNS(null, 'width', 13 * factor);
       image.setAttributeNS(null, 'height', 13 * factor);
-      image.setAttributeNS(null, 'href', `https://unpkg.com/ch-canton-symbols@1.3.1/symbols/13x13/${id.toLowerCase()}.svg`);
+      image.setAttributeNS(null, 'href', cantonSymbolUrl(id));
       pattern.append(image);
       return `url(#${patternId})`;
     });
   });
-  return cantons
+  return patterns
 }
 
 function initializeMap() {
@@ -60,7 +61,7 @@ function swisstopoGrayMap() {
   return L.tileLayer.swiss({ layer: 'ch.swisstopo.pixelkarte-grau' });
 }
 
-function cantonalBoundaries(cantonPatterns) {
+function cantonalBoundaries() {
   return new VectorTileLayer('cantonal-boundaries/{z}/{x}/{y}.pbf', {
     attribution: `
       <a href="https://github.com/nzzdev/ch-canton-symbols">NZZ Storytelling</a>
@@ -70,14 +71,13 @@ function cantonalBoundaries(cantonPatterns) {
     getFeatureId(feature) {
       return feature.properties.name
     },
-    maxNativeZoom: 20,
-    style: makeCantonStyle(cantonPatterns)
+    maxNativeZoom: 20
   });
 }
 
-function makeCantonStyle(cantonPatterns, highlightId) {
+function makeCantonStyle(cantonPatterns, highlightCanton) {
   return (feature, layerName, zoom) => {
-    const id = feature.properties.name;
+    const name = feature.properties.name;
     let patternSize = 0;
     let weight = 1;
     if (zoom > 20) {
@@ -88,11 +88,50 @@ function makeCantonStyle(cantonPatterns, highlightId) {
       weight = 2;
     }
     return {
-      color: '#000',
-      fill: highlightId !== id,
-      fillColor: cantonPatterns[id][patternSize],
+      color: '#333',
+      fill: highlightCanton !== name,
+      fillColor: cantonPatterns[name][patternSize],
       fillOpacity: 1,
-      weight: weight * window.devicePixelRatio,
+      weight: weight,
     };
   };
+}
+
+function highlightOnClick(overlayLayer, state) {
+  overlayLayer.on('click', event => {
+    state.highlightCanton = event.layer.properties.name;
+    updateOverlay(overlayLayer, state);
+  });
+}
+
+function updateOverlay(overlayLayer, state) {
+  overlayLayer.setStyle(makeCantonStyle(cantonPatterns, state.highlightCanton));
+  const overlay = document.querySelector('.overlay');
+  if (state.highlightCanton) {
+    const flag = new Image(13, 13);
+    flag.alt = `Flag of ${state.highlightCanton}`;
+    flag.src = cantonSymbolUrl(cantonIdByName[state.highlightCanton]);
+    const caption = document.createTextNode(state.highlightCanton);
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.setAttributeNS(null, 'role', 'button');
+    svg.setAttributeNS(null, 'aria-label', 'Close');
+    svg.setAttributeNS(null, 'viewBox', '0 0 32 32');
+    svg.setAttributeNS(null, 'width', 16);
+    svg.setAttributeNS(null, 'height', 16);
+    svg.addEventListener('click', () => {
+      state.highlightCanton = null;
+      updateOverlay(overlayLayer, state);
+    });
+    const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path.setAttributeNS(null, 'd', 'M24 10L22 8L16 14L10 8L8 10L14 16L8 22L10 24L16 18L22 24L24 22L18 16L24 10Z');
+    path.setAttributeNS(null, 'fill', '#000');
+    svg.append(path);
+    overlay.replaceChildren(flag, caption, svg);
+  } else {
+    overlay.textContent = 'Tap canton to highlight…';
+  }
+}
+
+function cantonSymbolUrl(id) {
+  return `https://unpkg.com/ch-canton-symbols@1.3.1/symbols/13x13/${id.toLowerCase()}.svg`;
 }
